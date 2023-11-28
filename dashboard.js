1a28374f10e5a899f4ad767a9b869c5a2caf47a2
Student <alebarriasanchez@gmail.com>
2023-11-28 15:10:30 +0100

    before trying the popup

--- dashboard.js
+++ dashboard.js
@@ -1,8 +1,8 @@
 const createNewOffer = document.querySelector(".new-offer");
 
-createNewOffer.addEventListener("click", (e) => {
-  console.log("Button was clicked");
-});
+// createNewOffer.addEventListener("click", (e) => {
+//   console.log("Button was clicked");
+// });
 const btn = document.querySelector(".new-offer");
 btn.addEventListener("click", (e) => {
   window.location.assign("proInformation.html");
@@ -10,3 +10,106 @@ btn.addEventListener("click", (e) => {
 // function visitPage() {
 //   window.location.assign("proTeam.html");
 // }
+// below go get the info
+let storedData = localStorage.getItem("formData");
+let displayElement = document.getElementById("display-data");
+
+// to create the new element
+let projects = JSON.parse(storedData);
+if (storedData === null) {
+  displayElement.style.display = "none";
+} else {
+  projects.forEach((project) => {
+    const createCard = document.createElement("div");
+    createCard.classList.add("cart");
+
+    const createInElement = document.createElement("div");
+    createInElement.classList.add("cart-content");
+
+    createCard.appendChild(createInElement);
+
+    createInElement.innerHTML = `
+    <h4>${project.name}  </h4>
+<div>
+
+  <span> Client</span>  <p>${project.client}</p>    
+  
+  </div>
+  <div>
+  <span>Team</span> <p></p>
+  </div>
+  <div class="cart-footer">
+              <div class="approved">
+                <img src="/images/Pending.svg" alt="pending icon" />
+                <span>Approved</span>
+              </div>
+              <img src="/images/Delete.svg" alt="delete icon" />
+              <button class="removeCard">delete</button>
+            </div>`;
+    displayElement.append(createCard);
+  });
+}
+//this work well but only for the newly created card
+/* let removeBtn = document.getElementById("removeCard");
+let removeCard = document.getElementById("display-data");
+removeBtn.addEventListener("click", function () {
+  removeCard.remove();
+}); */
+
+// trying with chat gpt
+
+// Add a click event listener to the parent container
+displayElement.addEventListener("click", function (event) {
+  // Check if the clicked element is the delete button
+  if (event.target.classList.contains("removeCard")) {
+    // Find the parent card and remove it
+    const cardToRemove = event.target.closest(".cart");
+    if (cardToRemove) {
+      cardToRemove.remove();
+    }
+  }
+});
+
+// Add a click event listener to the existing delete buttons
+document.querySelectorAll(".removeCard").forEach(function (button) {
+  button.addEventListener("click", function () {
+    // Find the parent card and remove it
+    const cardToRemove = button.closest(".cart");
+    if (cardToRemove) {
+      cardToRemove.remove();
+    }
+  });
+});
+
+/* let removeOldCards = document.querySelector("cart");
+removeBtn.addEventListener("click", function () {
+  removeOldCards.remove();
+}); */
+
+// code below works. it was a first try
+
+/* let projects = JSON.parse(storedData);
+if (storedData === null) {
+  displayElement.style.display = "none";
+} else {
+  projects.forEach((project) => {
+    const createElement = document.createElement("div");
+    createElement.classList.add("cart");
+    const createInElement = document.createElement("div");
+    createInElement.classList.add("cart-content");
+
+    createElement.appendChild(createInElement);
+
+    createInElement.innerHTML = `
+    <h4>${project.name}  </h4>
+<div>
+
+  <span> Client</span>  <p>${project.client}</p>    
+  
+  </div>
+  <div>
+  <span>Team</span> <p></p>
+  </div>`;
+    displayElement.append(createElement);
+  });
+} */
--- proInformation.js
+++ proInformation.js
@@ -1,16 +1,90 @@
-const project = document.querySelector("#project");
-project.addEventListener("blur", (e) => {
-  console.log(e.target.value, {});
-});
+// const project = document.querySelector("#project");
+// project.addEventListener("click", );
 
-const offer = document.querySelector("#offer");
-offer.addEventListener("blur", (e) => {
-  console.log(e.target.value, {});
-});
+// const offer = document.querySelector("#offer");
+// offer.addEventListener("blur", (e) => {
+//   console.log(e.target.value, {});
+// });
 
-const client = document.querySelector("#client");
-client.addEventListener("blur", (e) => {
-  console.log(e.target.value, {});
-});
+// const client = document.querySelector("#client");
+// client.addEventListener("blur", (e) => {
+//   console.log(e.target.value, {});
+// });
 
-localStorage.setItem(project, offer, client);
+// localStorage.setItem(project, offer, client);
+
+// code with gtp
+
+// document.getElementById("project").addEventListener("keyup", function (event) {
+//   // Check if the key pressed is "Enter" (key code 13)
+//   if (event.key === "Enter") {
+//     saveData();
+//   }
+// });
+
+// document.getElementById("offer").addEventListener("keyup", function (event) {
+//   // Check if the key pressed is "Enter" (key code 13)
+//   if (event.key === "Enter") {
+//     saveData();
+//   }
+// });
+
+// function saveData() {
+//   let inputData = document.getElementById("registration-form").value;
+
+//   if (inputData.trim() !== "") {
+//     localStorage.setItem("userData", inputData);
+//     alert("Data saved successfully!");
+//   } else {
+//     alert("Please enter data before saving.");
+//   }
+// }
+// trying to gather all the values
+
+document
+  .getElementById("registration-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const projectName = document.getElementById("project").value;
+    const offerName = document.getElementById("offer").value;
+    const clientName = document.getElementById("client").value;
+    const contactPerson = document.getElementById("person-name").value;
+    const eMail = document.getElementById("email").value;
+    const street = document.getElementById("street").value;
+    const city = document.getElementById("city").value;
+    const postal = document.getElementById("postal").value;
+    const projectDescription = document.getElementById(
+      "project-description"
+    ).value;
+
+    const formData = {
+      name: projectName,
+      offer: offerName,
+      client: clientName,
+      contactPerson: contactPerson,
+      email: eMail,
+      street: street,
+      city: city,
+      postalCode: postal,
+      description: projectDescription,
+    };
+
+    console.log(formData);
+
+    saveFormData(formData);
+    window.location.href = "index.html";
+  });
+
+function saveFormData(formData) {
+  const storedFormData = JSON.parse(localStorage.getItem("formData")) || [];
+
+  storedFormData.push(formData);
+
+  localStorage.setItem("formData", JSON.stringify(storedFormData));
+}
+
+// const btnNext = document.querySelector(".arrow-next");
+// btnNext.addEventListener("click", (e) => {
+//   window.location.assign("proCost.html");
+// });
